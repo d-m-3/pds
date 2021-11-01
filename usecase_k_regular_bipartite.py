ba52e07bb0ee0b549783521800d999217189d7b5
d-m-3 <dimitri.gurtner@gmail.com>
2021-11-02 00:00:13 +0100

    Added: k-regular bipartite graphs. Modified: Framework generalized to k-regulars graphs, instead of 3-regular graphs.

--- usecase_k_regular_bipartite.py
+++ usecase_k_regular_bipartite.py
@@ -11,17 +11,17 @@ def pds_k_regular_bipartite_algorithm(BG):
         # First neighbor of u in Y.
         v_Y = list(BG.neighbors(u))[0] 
         neighbors_v_Y = list(BG.neighbors(v_Y))
-        # Remove u from v_Y
+        # Remove u from v_Y.
         neighbors_v_Y.remove(u)
         for v_X in neighbors_v_Y:
             if len(sorted(nx.common_neighbors(BG, u, v_X))) == 1:
                 # u and v_Y have only v_Y as a common neighbor.
-                # Add u, v_X, v_Y in \bar{PDS}
+                # Add u, v_X, v_Y in \bar{PDS}.
                 pds_bar = [u, v_X, v_Y]
                 break
-    # PDS = V - pds_bar
+    # PDS = V - pds_bar.
     pds_list = [v for v in list(BG.nodes) if v not in pds_bar]
-    # Look for a vertex s in PDS that has all its neighbors in PDS
+    # Look for a vertex s in PDS that has all its neighbors in PDS.
     for s in pds_list:
         if len(pds_list) == pds.pds_size(BG.number_of_nodes(), BG.degree[0]):
             break
@@ -29,29 +29,34 @@ def pds_k_regular_bipartite_algorithm(BG):
         neighbors_s = list(BG.neighbors(s))
         if all(v in pds_list for v in neighbors_s):
             # We will remove one neighbor of s that has also all its
-            # neighbors in PDS
+            # neighbors in PDS.
             for t in neighbors_s:
                 neighbors_t = list(BG.neighbors(t))
                 if all(v in pds_list for v in neighbors_t):
-                    #t can be removed from PDS, and appended in \bar{PDS}
+                    #t can be removed from PDS, and appended in \bar{PDS}.
                     pds_list.remove(t)
                     pds_bar.append(t)
                     print(t)
                     break
     return pds_list
 
+
 def main():
-    n = 18
-    k = 4
-    for _ in range(100):
+    n = 24
+    k = 3
+    graphs_nb = 100
+    for i in range(1, graphs_nb + 1):
         BG = pds.get_k_regular_bipartite_graph(n, k)
         max_pds = pds.find_one_max_pds(BG)
         if len(max_pds) == 0:
             print("Exception")
             pds.draw_graph(BG, max_pds, bipartite_layout=True)
             break
+        # If no exception could be found, draw the last graph with its PDS.
+        pds.display_progress(i, graphs_nb)
+    pds.draw_graph(BG, max_pds, bipartite_layout=True)
     
-    # Algorithm 26.10.2021. DOES NOT WORK
+    # Algorithm 26.10.2021. DOES NOT WORK.
     '''
     pds_list = pds_k_regular_bipartite_algorithm(BG)
     is_pds = pds.is_subgraph_a_pds(BG, pds_list)
