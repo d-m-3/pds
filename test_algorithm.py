90ca009e3fc48e6a843948075e84d67d602e362a
d-m-3 <dimitri.gurtner@gmail.com>
2021-11-02 23:10:57 +0100

    Added: Usecase check if a subset is a PDS. Modified: Graph from file.

---
+++ test_algorithm.py
@@ -0,0 +1,77 @@
+import networkx as nx
+import pds
+
+def pds_k_regular_bipartite_algorithm(BG):
+    """
+    Returns a PDS of the maximum size. Algorithm 26.10.2021.
+    DOES NOT WORK.
+    """
+    X = {n for n, d in BG.nodes(data=True) if d["bipartite"] == 0}
+    for u in list(X):
+        # First neighbor of u in Y.
+        v_Y = list(BG.neighbors(u))[0] 
+        neighbors_v_Y = list(BG.neighbors(v_Y))
+        # Remove u from v_Y.
+        neighbors_v_Y.remove(u)
+        for v_X in neighbors_v_Y:
+            if len(sorted(nx.common_neighbors(BG, u, v_X))) == 1:
+                # u and v_Y have only v_Y as a common neighbor.
+                # Add u, v_X, v_Y in \bar{PDS}.
+                pds_bar = [u, v_X, v_Y]
+                print(u, v_X, v_Y)
+                break
+        break
+    # pds_list = V - pds_bar.
+    pds_list = [v for v in list(BG.nodes) if v not in pds_bar]
+    
+    # UNFINISHED.
+    # Set of neighbors of u, v_X, v_Y.
+    set_neigh = set(list(BG.neighbors(u)) + list(BG.neighbors(v_X)) 
+                    + list(BG.neighbors(v_Y)))
+    set_neigh.difference_update(pds_bar)
+    print(set_neigh)
+    #Should only work for 12 vertices for the moment.
+    for v4 in set_neigh:
+        neighbors_v4 = set(list(BG.neighbors(v4)))
+        neighbors_v4.difference_update(pds_bar)
+        print("N(v4)", v4, neighbors_v4)
+        if all(v in pds_list for v in neighbors_v4):
+            pds_list.remove(v4)
+            pds_bar.append(v4)
+            break
+    return pds_list
+
+    '''
+    # Look for a vertex s in PDS that has all its neighbors in PDS.
+    for s in pds_list:
+        # Stop if the PDS has the maximum size.
+        if len(pds_list) == pds.pds_size(BG.number_of_nodes(), BG.degree[0]):
+            break
+        # Check if PDS contains all vertices of neighbors of s.
+        neighbors_s = list(BG.neighbors(s))
+        if all(v in pds_list for v in neighbors_s):
+            # We will remove one neighbor of s that has also all its
+            # neighbors in PDS.
+            for t in neighbors_s:
+                neighbors_t = list(BG.neighbors(t))
+                if all(v in pds_list for v in neighbors_t):
+                    #t can be removed from PDS, and appended in \bar{PDS}.
+                    pds_list.remove(t)
+                    pds_bar.append(t)
+                    break
+    return pds_list
+    '''
+
+def main():
+    n = 12
+    k = 3
+    # Algorithm 26.10.2021. DOES NOT WORK.
+    BG = pds.get_k_regular_bipartite_graph(n, k)
+    pds_list = pds_k_regular_bipartite_algorithm(BG)
+    pds.draw_graph(BG, pds_list, bipartite_layout=True)
+    is_pds = pds.is_subgraph_a_pds(BG, pds_list)
+    print("is PDS?", is_pds)
+    print(pds_list)
+
+if __name__ == '__main__':
+    main()
--- usecase_k_regular_bipartite.py
+++ usecase_k_regular_bipartite.py
@@ -1,49 +1,18 @@
-import networkx as nx
 import pds
-
-def pds_k_regular_bipartite_algorithm(BG):
-    """
-    Returns a PDS of the maximum size. Algorithm 26.10.2021.
-    DOES NOT WORK.
-    """
-    X = {n for n, d in BG.nodes(data=True) if d["bipartite"] == 0}
-    for u in list(X):
-        # First neighbor of u in Y.
-        v_Y = list(BG.neighbors(u))[0] 
-        neighbors_v_Y = list(BG.neighbors(v_Y))
-        # Remove u from v_Y.
-        neighbors_v_Y.remove(u)
-        for v_X in neighbors_v_Y:
-            if len(sorted(nx.common_neighbors(BG, u, v_X))) == 1:
-                # u and v_Y have only v_Y as a common neighbor.
-                # Add u, v_X, v_Y in \bar{PDS}.
-                pds_bar = [u, v_X, v_Y]
-                break
-    # PDS = V - pds_bar.
-    pds_list = [v for v in list(BG.nodes) if v not in pds_bar]
-    # Look for a vertex s in PDS that has all its neighbors in PDS.
-    for s in pds_list:
-        if len(pds_list) == pds.pds_size(BG.number_of_nodes(), BG.degree[0]):
-            break
-        # Check if PDS contains all vertices of neighbors of s.
-        neighbors_s = list(BG.neighbors(s))
-        if all(v in pds_list for v in neighbors_s):
-            # We will remove one neighbor of s that has also all its
-            # neighbors in PDS.
-            for t in neighbors_s:
-                neighbors_t = list(BG.neighbors(t))
-                if all(v in pds_list for v in neighbors_t):
-                    #t can be removed from PDS, and appended in \bar{PDS}.
-                    pds_list.remove(t)
-                    pds_bar.append(t)
-                    print(t)
-                    break
-    return pds_list
 
 
 def main():
     n = 12
-    k = 3
+    k = 4
+    
+    # Draw a k-regular bipartite graph with its PDS of the maximum size.
+    '''
+    BG = pds.get_k_regular_bipartite_graph(n, k)
+    max_pds = pds.find_one_max_pds(BG)
+    pds.draw_graph(BG, max_pds, bipartite_layout=True)
+    '''
+    
+    # Trying to find a graph that has not a PDS of the maximum size.
     graphs_nb = 100
     for i in range(1, graphs_nb + 1):
         BG = pds.get_k_regular_bipartite_graph(n, k)
@@ -56,13 +25,5 @@ def main():
         pds.display_progress(i, graphs_nb)
     pds.draw_graph(BG, max_pds, bipartite_layout=True)
     
-    # Algorithm 26.10.2021. DOES NOT WORK.
-    '''
-    pds_list = pds_k_regular_bipartite_algorithm(BG)
-    is_pds = pds.is_subgraph_a_pds(BG, pds_list)
-    print("is PDS?", is_pds)
-    print(pds_list)
-    '''
-
 if __name__ == '__main__':
     main()
