2a43e6ae6833904535b702f2ebfec895a69cce2f
d-m-3 <dimitri.gurtner@gmail.com>
2022-03-14 11:17:25 +0100

    Modified: unit tests redesign (a class per function).

--- example_unbalanced_pds.py
+++ example_unbalanced_pds.py
@@ -2,7 +2,11 @@ import pds
 import gex
 
 def main():
-
+    """
+    This file shows and draws a specific cubic graph on 18 vertices with
+    a PDS that has not the same number of vertices in independent sets
+    X and Y, expressed here as an "unbalanced PDS", which is unusual.
+    """
     G = gex.G_cubic_bipartite_unbalanced_pds()
     subgraph = [0, 1, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17]
     is_sub_a_pds = pds.is_subgraph_a_pds(G, subgraph)
